e2ce788a424a526ddb020dcaec591e621db2c824
EliasMoran <yoexereliasmoranurcia@gmail.com>
2020-07-06 20:35:44 -0500

    SEMANA 02: Implementando interfaces de usuario: Layouts y RaisedButton

---
+++ app/src/main/java/com/example/petagram/ListadoMascotas.java
@@ -0,0 +1,17 @@
+package com.example.petagram;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListadoMascotas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //mostar la interfaz gráfica
+        setContentView(R.layout.activity_listado_mascotas);
+
+
+    }
+}
