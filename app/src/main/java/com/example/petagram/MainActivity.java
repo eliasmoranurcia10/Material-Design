60e0882b8782ebf17c6b565daab1a2aa59570b1c
EliasMoran <yoexereliasmoranurcia@gmail.com>
2020-07-06 22:13:03 -0500

    SEMANA 02: Floating Action Button

--- app/src/main/java/com/example/petagram/MainActivity.java
+++ app/src/main/java/com/example/petagram/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.petagram;
 
 import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -14,8 +18,19 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        agregarFAB();
 
 
+    }
+
+    public void agregarFAB(){
+        FloatingActionButton miFAB = (FloatingActionButton) findViewById(R.id.fabMiFab);
+        miFAB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 
 }
