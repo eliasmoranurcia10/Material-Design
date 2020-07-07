2551db39b0f1358e892253dcd8d73398e20c7b3e
EliasMoran <yoexereliasmoranurcia@gmail.com>
2020-07-07 02:24:00 -0500

    SEMANA 02: Refresh Indicator; Refrescando paginas

--- app/src/main/java/com/example/petagram/MainActivity.java
+++ app/src/main/java/com/example/petagram/MainActivity.java
@@ -3,16 +3,26 @@ package com.example.petagram;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import java.util.ArrayList;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    SwipeRefreshLayout sfiMiIndicadorRefresh;
+    ListView lstmiLista;
+    Adapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +33,29 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         agregarFAB();
 
+        //Encontrar el id del SwipeRefresh
+        sfiMiIndicadorRefresh = (SwipeRefreshLayout) findViewById(R.id.sfiMiIndicadorRefresh);
+        //Encontrar el id de la lista
+        lstmiLista = (ListView) findViewById(R.id.lstMiLista);
 
+        //crear un array con lista de strings
+        String[] planetas = getResources().getStringArray(R.array.planetas);
+
+        //Adaptar la lista
+        lstmiLista.setAdapter(new ArrayAdapter(this,android.R.layout.simple_list_item_1,planetas));
+        //Cuando se resetea, estar atento a un reseteo
+        sfiMiIndicadorRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                refrescandoContenido();
+            }
+        });
+    }
+
+    public void refrescandoContenido(){
+        String[] planetas = getResources().getStringArray(R.array.planetas);
+        lstmiLista.setAdapter(new ArrayAdapter(this,android.R.layout.simple_list_item_1,planetas));
+        sfiMiIndicadorRefresh.setRefreshing(false);
     }
 
     public void agregarFAB(){
